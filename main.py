0934f7dff3c040213f0d768603be743e9bf483b8
rathore526 <kr3612594@gmail.com>
2026-09-18 10:24:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,23 +14,8 @@ def run():
 
 def keep_alive():
     t = Thread(target=run)
-    t.start()import os
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is alive!"
-
-def run():
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
-
-def keep_alive():
-    t = Thread(target=run)
     t.start()
+
 import discord
 from discord.ext import commands
 import asyncio
